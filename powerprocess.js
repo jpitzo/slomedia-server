847d3f37b2ce76e8326e1e950da7dd7254de743f
Joey Pitzo <pitzoner@gmail.com>
2016-02-25 00:55:04 -0800

    log to file

--- powerprocess.js
+++ powerprocess.js
@@ -1,4 +1,15 @@
 // Process to manage powermate.js
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
+
 var pm = require('./powermate');
 var powermate = null;
 var pulsing = false;
